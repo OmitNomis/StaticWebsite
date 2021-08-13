27ff525744a7dd4ce8bce0170a5102a38d13a1b6
OmitNomis <87754854+OmitNomis@users.noreply.github.com>
2021-08-13 11:51:36 +0545

    Add files via upload

---
+++ JavaScript/javascript.js
@@ -0,0 +1,119 @@
+
+
+       
+function show1() { //function to display personal information
+       
+       document.getElementById("1").style.display = "block"; //show personal information
+       document.getElementById("2").style.display = "none"; //hide Education and Qualification
+       document.getElementById("3").style.display = "none"; //hide skills
+       
+
+     }
+
+function show2() {//function to display Education and Qualification
+       
+       document.getElementById("2").style.display = "block";//Show Education and Qualification
+       document.getElementById("1").style.display = "none";//hide Personal Information
+       document.getElementById("3").style.display = "none";//hide skills
+       
+     }
+
+function show3() {//function to display skills
+       
+       document.getElementById("3").style.display = "block";//show skills
+       document.getElementById("2").style.display = "none";//hide Education and Qualification
+       document.getElementById("1").style.display = "none";//hide Personal Information
+       
+     }
+      
+       
+  function showIntro() {//function to display introduction
+       
+       document.getElementById("1").style.display = "block";//show introduction
+       document.getElementById("2").style.display = "none";//hide advantages
+       document.getElementById("3").style.display = "none";//hide disadvantages
+       document.getElementById("4").style.display = "none";//hide Verdict
+       
+
+     }
+
+function showAdv() {//function to display Advantages
+       
+       document.getElementById("2").style.display = "block";//show Advantages
+       document.getElementById("1").style.display = "none";//hide introduction
+       document.getElementById("3").style.display = "none";//hide disadvantages
+       document.getElementById("4").style.display = "none";//hide Verdict
+       
+     }
+
+function showDisadv() {//function to display disadvantages
+       
+       document.getElementById("3").style.display = "block";//show disadvantages
+       document.getElementById("2").style.display = "none";//hide Advantages
+       document.getElementById("1").style.display = "none";//hide introduction
+       document.getElementById("4").style.display = "none";//hide Verdict
+       
+     }
+      
+
+function showVerdict() {//function to display Verdict
+       
+       document.getElementById("4").style.display = "block";//show Verdict
+       document.getElementById("2").style.display = "none";//hide Advantages
+       document.getElementById("1").style.display = "none";//hide introduction
+       document.getElementById("3").style.display = "none";//hide disadvantages
+       
+     }
+
+
+  function research1() {//function to display research 1
+       
+       document.getElementById("1").style.display = "block";//show research 1
+       document.getElementById("2").style.display = "none";//hide research 2
+       document.getElementById("3").style.display = "none";//hide research 3
+       document.getElementById("4").style.display = "none";//hide research 4
+       document.getElementById("5").style.display = "none";//hide research 5
+       
+
+     }
+
+function research2() {//function to display research 2
+       
+       document.getElementById("2").style.display = "block";//show research 2
+       document.getElementById("1").style.display = "none";//hide research 1
+       document.getElementById("3").style.display = "none";//hide research 3
+       document.getElementById("4").style.display = "none";//hide research 4
+       document.getElementById("5").style.display = "none";//hide research 5
+       
+     }
+
+function research3() {//function to display research 3
+       
+       document.getElementById("3").style.display = "block";//show research 3
+       document.getElementById("2").style.display = "none";//hide research 2
+       document.getElementById("1").style.display = "none";//hide research 1
+       document.getElementById("4").style.display = "none";//hide research 4
+       document.getElementById("5").style.display = "none";//hide research 5
+       
+     }
+      
+
+
+function research4() {//function to display research 4
+       
+       document.getElementById("4").style.display = "block";//show research 4
+       document.getElementById("2").style.display = "none";//hide research 2
+       document.getElementById("1").style.display = "none";//hide research 1
+       document.getElementById("3").style.display = "none";//hide research 3
+       document.getElementById("5").style.display = "none";//hide research 5
+       
+     }
+
+     function research5() {//function to display research 5
+       
+       document.getElementById("5").style.display = "block";//show research 5
+       document.getElementById("2").style.display = "none";//hide research 2
+       document.getElementById("1").style.display = "none";//hide research 1
+       document.getElementById("3").style.display = "none";//hide research 3
+       document.getElementById("4").style.display = "none";//hide research 4
+     }
